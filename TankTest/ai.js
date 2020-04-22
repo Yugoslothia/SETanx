abf0ef2157ae672becadee99e6df41d54439c537
Yugoslothia <46723398+Yugoslothia@users.noreply.github.com>
2020-04-22 00:31:43 -0400

    Ai Collision and Implementation
    Got collision to work with bullets and adapted it to the 1st level

--- TankTest/ai.js
+++ TankTest/ai.js
@@ -60,8 +60,9 @@ class Ai extends Phaser.Physics.Arcade.Sprite {
     }
   }
 
+  // shoots in the direction of the player and spawns unique bullet
   shootAI(){
-		var ebullet = enemyBullets.create(this.x, this.y);
+		var ebullet = enemyBullets.create(this.x, this.y).setScale(0.3);
     var direction = Math.atan((player.y-this.y)/(player.x-this.x));
     ebullet.setVelocityX(175 * Math.cos(direction));
     ebullet.setVelocityY(175 * Math.sin(direction));
@@ -77,9 +78,10 @@ class Ai extends Phaser.Physics.Arcade.Sprite {
   }
 
   aiKill(bullet) {
+    bullet.x = -100;
+    bullet.y = -100;
     this.disableBody(true, true);
     this.aiHead.disableBody(true, true);
-    bullet.disableBody(true, true);
     this.timedEvent.remove();
     this.dead = true;
   }
@@ -113,7 +115,7 @@ class AiFastBullet extends Ai {
   }
 
   shootAI(){
-    var ebullet = enemyBullets.create(this.x, this.y);
+    var ebullet = enemyBullets.create(this.x, this.y).setScale(0.3);
     var direction = Math.atan((player.y-this.y)/(player.x-this.x));
     ebullet.setVelocityX(300 * Math.cos(direction));
     ebullet.setVelocityY(300 * Math.sin(direction));
@@ -243,94 +245,4 @@ class EvilChris extends Phaser.Physics.Arcade.Sprite {
 
     isDead() {return this.dead;}
 
-
 }
-/*
-class AiStill extends Phaser.Physics.Arcade.Sprite {
-
-  constructor (scene, x, y) {
-    super(scene, x, y, 'enemy2');
-    scene.add.existing(this);
-    scene.physics.world.enable(this);
-    this.body.setCollideWorldBounds(true);
-    scene.physics.add.collider(this, walls);
-    this.aiHead = new AiHead(scene, this.x, this.y);
-    this.dead = false;
-    scene.physics.add.collider(this, bullets, this.aiKill, null, this);
-
-    this.timedEvent = scene.time.addEvent({ delay: 2000, callback: this.shootAI, callbackScope: this, loop: true });
-  }
-
-  preUpdate(time, delta) {
-    super.preUpdate(time, delta);
-    this.aiHead.rotation = Phaser.Math.Angle.Between(this.aiHead.x, this.aiHead.y, player.x, player.y);
-    this.aiHead.x = this.x;
-    this.aiHead.y = this.y;
-  }
-
-    // randomly moves ai whenever it moves 100 pixels in y or x dir on dir change
-    if(this.x - this.oldx > 100 || this.x - this.oldx < -100 || this.y - this.oldy > 100 || this.y - this.oldy < -100
-        || (this.x == this.oldx && this.y == this.oldy)) {
-      this.dirx = Phaser.Math.Between(-1, 1);
-      this.diry = Phaser.Math.Between(-1, 1);
-      while(this.dirx == 0 && this.diry == 0) {
-        this.dirx = Phaser.Math.Between(-1, 1);
-        this.diry = Phaser.Math.Between(-1, 1);
-      }
-      if(Math.abs(this.dirx) == Math.abs(this.diry)) {
-        this.dirx *= Math.sqrt(2) / 2;
-        this.diry *= Math.sqrt(2) / 2;
-      }
-      this.setVelocityX(100 * this.dirx);
-      this.setVelocityY(100 * this.diry);
-      this.oldx = this.x;
-      this.oldy = this.y;
-    }
-  }
-
-  shootAI(){
-		var ebullet = enemyBullets.create(this.x, this.y);
-    var direction = Math.atan((player.y-this.y)/(player.x-this.x));
-    ebullet.setVelocityX(175 * Math.cos(direction));
-    ebullet.setVelocityY(175 * Math.sin(direction));
-    if(player.x - this.x < 0 && player.y - this.y >= 0) {
-      ebullet.setVelocityY(-175 * Math.sin(direction));
-      ebullet.setVelocityX(-175 * Math.cos(direction));
-    }
-    if(player.x - this.x < 0 && player.y - this.y < 0) {
-      ebullet.setVelocityY(-175 * Math.sin(direction));
-      ebullet.setVelocityX(-175 * Math.cos(direction));
-    }
-    ebullet.body.setCollideWorldBounds(true).setBounce(1);
-  }
-
-  aiKill(bullets) {
-    this.disableBody(true, true);
-    this.aiHead.disableBody(true, true);
-    bullets.disableBody(true, true);
-    this.timedEvent.remove();
-    this.dead = true;
-  }
-}
-
-/*
-class Bullet extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(scene, x, y, "bullet");
-    scene.add.existing(this);
-    scene.physics.world.enable(this);
-    scene.physics.collider(this, walls);
-    this.body.setCollideWorldBounds(true).bounce.set(1);
-    var direction = Math.atan((player.y-this.y)/(player.x-this.x));
-    this.setVelocityX(175 * Math.cos(direction));
-    this.setVelocityY(175 * Math.sin(direction));
-    if(player.x - this.x < 0 && player.y - this.y >= 0) {
-      this.setVelocityY(-175 * Math.sin(direction));
-      this.setVelocityX(-175 * Math.cos(direction));
-    }
-    if(player.x - this.x < 0 && player.y - this.y < 0) {
-      this.setVelocityY(-175 * Math.sin(direction));
-      this.setVelocityX(-175 * Math.cos(direction));
-    }
-  }
-}*/
